83c4abe8211af7d54ab54aa4f57af2c98e972da5
maks <metelski.max@gmail.com>
2021-04-13 21:34:25 +0500

    Изменён внешний вид формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,30 +36,37 @@ namespace AdmUsrPwd
             this.txtPasswd = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label4 = new System.Windows.Forms.Label();
             this.btnUsrSearch = new System.Windows.Forms.Button();
             this.lstBoxUsr = new System.Windows.Forms.ListBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtUserPhone = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(163, 46);
+            this.txtUserName.Location = new System.Drawing.Point(146, 31);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(157, 20);
+            this.txtUserName.Size = new System.Drawing.Size(208, 22);
             this.txtUserName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label1.Location = new System.Drawing.Point(54, 46);
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.Size = new System.Drawing.Size(131, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Имя пользователя";
             // 
@@ -67,35 +74,39 @@ namespace AdmUsrPwd
             // 
             this.label2.AutoSize = true;
             this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label2.Location = new System.Drawing.Point(169, 207);
+            this.label2.Location = new System.Drawing.Point(13, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(153, 17);
             this.label2.TabIndex = 5;
-            this.label2.Text = "имя рабочей станции";
+            this.label2.Text = "Имя рабочей станции";
             // 
             // txtCompName
             // 
-            this.txtCompName.Location = new System.Drawing.Point(174, 223);
+            this.txtCompName.Location = new System.Drawing.Point(172, 35);
+            this.txtCompName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCompName.Name = "txtCompName";
             this.txtCompName.ReadOnly = true;
-            this.txtCompName.Size = new System.Drawing.Size(134, 20);
+            this.txtCompName.Size = new System.Drawing.Size(272, 22);
             this.txtCompName.TabIndex = 6;
             // 
             // txtPasswd
             // 
-            this.txtPasswd.Location = new System.Drawing.Point(400, 223);
+            this.txtPasswd.Location = new System.Drawing.Point(517, 35);
+            this.txtPasswd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPasswd.Name = "txtPasswd";
             this.txtPasswd.ReadOnly = true;
-            this.txtPasswd.Size = new System.Drawing.Size(131, 20);
+            this.txtPasswd.Size = new System.Drawing.Size(265, 22);
             this.txtPasswd.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label3.Location = new System.Drawing.Point(418, 207);
+            this.label3.Location = new System.Drawing.Point(452, 38);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.Size = new System.Drawing.Size(57, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "Пароль";
             // 
@@ -104,28 +115,22 @@ namespace AdmUsrPwd
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(163, 299);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(533, 150);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1158, 295);
             this.dataGridView1.TabIndex = 11;
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label4.Location = new System.Drawing.Point(375, 269);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 13);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "История поисков";
-            // 
             // btnUsrSearch
             // 
-            this.btnUsrSearch.Location = new System.Drawing.Point(601, 40);
+            this.btnUsrSearch.Location = new System.Drawing.Point(690, 29);
+            this.btnUsrSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUsrSearch.Name = "btnUsrSearch";
-            this.btnUsrSearch.Size = new System.Drawing.Size(113, 23);
+            this.btnUsrSearch.Size = new System.Drawing.Size(151, 28);
             this.btnUsrSearch.TabIndex = 14;
             this.btnUsrSearch.Text = "Найти";
             this.btnUsrSearch.UseVisualStyleBackColor = true;
@@ -133,10 +138,13 @@ namespace AdmUsrPwd
             // 
             // lstBoxUsr
             // 
+            this.lstBoxUsr.Dock = System.Windows.Forms.DockStyle.Top;
             this.lstBoxUsr.FormattingEnabled = true;
-            this.lstBoxUsr.Location = new System.Drawing.Point(178, 90);
+            this.lstBoxUsr.ItemHeight = 16;
+            this.lstBoxUsr.Location = new System.Drawing.Point(0, 78);
+            this.lstBoxUsr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstBoxUsr.Name = "lstBoxUsr";
-            this.lstBoxUsr.Size = new System.Drawing.Size(387, 95);
+            this.lstBoxUsr.Size = new System.Drawing.Size(1164, 196);
             this.lstBoxUsr.TabIndex = 15;
             this.lstBoxUsr.SelectedIndexChanged += new System.EventHandler(this.lstBoxUsr_SelectedIndexChanged);
             // 
@@ -144,24 +152,27 @@ namespace AdmUsrPwd
             // 
             this.label5.AutoSize = true;
             this.label5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label5.Location = new System.Drawing.Point(326, 50);
+            this.label5.Location = new System.Drawing.Point(362, 34);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.Size = new System.Drawing.Size(121, 17);
             this.label5.TabIndex = 16;
             this.label5.Text = "Номер телефона";
             // 
             // txtUserPhone
             // 
-            this.txtUserPhone.Location = new System.Drawing.Point(421, 46);
+            this.txtUserPhone.Location = new System.Drawing.Point(491, 31);
+            this.txtUserPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserPhone.Name = "txtUserPhone";
-            this.txtUserPhone.Size = new System.Drawing.Size(144, 20);
+            this.txtUserPhone.Size = new System.Drawing.Size(191, 22);
             this.txtUserPhone.TabIndex = 17;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(601, 220);
+            this.button1.Location = new System.Drawing.Point(790, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.Size = new System.Drawing.Size(187, 28);
             this.button1.TabIndex = 18;
             this.button1.Text = "Копировать пароль";
             this.button1.UseVisualStyleBackColor = true;
@@ -169,38 +180,76 @@ namespace AdmUsrPwd
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(748, 41);
+            this.button2.Location = new System.Drawing.Point(849, 29);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.Size = new System.Drawing.Size(151, 28);
             this.button2.TabIndex = 19;
-            this.button2.Text = "отчистить условия";
+            this.button2.Text = "Сбросить";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtPasswd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCompName);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 274);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1164, 76);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные пользователя";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUsrSearch);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtUserName);
+            this.groupBox2.Controls.Add(this.txtUserPhone);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1164, 78);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Поиск";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 350);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1164, 316);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "История поисков";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 541);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtUserPhone);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(1164, 666);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lstBoxUsr);
-            this.Controls.Add(this.btnUsrSearch);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txtPasswd);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtCompName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -212,13 +261,15 @@ namespace AdmUsrPwd
         private System.Windows.Forms.TextBox txtPasswd;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnUsrSearch;
         private System.Windows.Forms.ListBox lstBoxUsr;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtUserPhone;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
